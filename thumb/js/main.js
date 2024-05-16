6b0056e46afcca3d5d953dbedc8cb6150011fb6d
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-17 00:51:27 +0200

    [UPD] bonus 1

--- thumb/js/main.js
+++ thumb/js/main.js
@@ -22,3 +22,58 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+const thumbs=document.querySelector('.thumbs');
+const imgitems=document.querySelector('.item img');
+const prev=document.querySelector('.prev');
+const next=document.querySelector('.next');
+
+for(let element of images){
+    const div=document.createElement('div');
+    div.classList.add('thumb');
+    const img=document.createElement('img');
+    img.src=element.image;
+    div.append(img);
+    thumbs.append(div);
+    div.addEventListener('click', function(event){
+        const currentEleemt=event.target;
+        
+        for(let i=0;i<imagesDiv.length;i++){
+            imagesDiv[i].classList.remove('active');
+        }
+        div.classList.add('active');
+        imgitems.src=element.image;
+    });
+}
+
+const imagesDiv=[document.querySelector('.thumbs div:nth-child(3)'),document.querySelector('.thumbs div:nth-child(4)'),document.querySelector('.thumbs div:nth-child(5)'),document.querySelector('.thumbs div:nth-child(6)'),document.querySelector('.thumbs div:nth-child(7)')];
+document.querySelector('.thumbs div:nth-child(3)').classList.add('active')
+
+
+prev.addEventListener('click',function(){
+    const active=document.querySelector('.thumbs .active');
+    for(let i=0;i<imagesDiv.length;i++){
+        if(active.querySelector('img').src===imagesDiv[i].querySelector('img').src){
+            if(i==0)
+                i=5;
+            active.classList.remove('active');
+            imagesDiv[i-1].classList.add('active');
+            imgitems.src=imagesDiv[i-1].querySelector('img').src;
+        }
+    }
+})
+next.addEventListener('click',function(){
+    const active=document.querySelector('.thumbs .active');
+    for(let i=0;i<imagesDiv.length;i++){
+        if(active.querySelector('img').src===imagesDiv[i].querySelector('img').src){
+            if(i==4)
+                i=-1;
+            active.classList.remove('active');
+            imagesDiv[i+1].classList.add('active');
+            imgitems.src=imagesDiv[i+1].querySelector('img').src;
+            break;
+        }
+    }
+})
+
+
+
